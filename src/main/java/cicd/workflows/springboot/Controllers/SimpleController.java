6547f1ebf52deb4bc6cedc29a06b8bef46242162
Firas Khalife <khalifefiras29@gmail.com>
2023-09-22 00:21:31 +0300

    code fixes

--- src/main/java/cicd/workflows/springboot/Controllers/SimpleController.java
+++ src/main/java/cicd/workflows/springboot/Controllers/SimpleController.java
@@ -4,11 +4,13 @@ import cicd.workflows.springboot.Dto.IntroDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class SimpleController {
 
     @GetMapping("/intro")
