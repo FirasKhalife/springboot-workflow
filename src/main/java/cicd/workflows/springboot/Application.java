5722f7872682d7732969b86653ead0dd4c3976c1
Firas Khalife <khalifefiras29@gmail.com>
2023-09-24 15:04:43 +0300

    small change

--- src/main/java/cicd/workflows/springboot/Application.java
+++ src/main/java/cicd/workflows/springboot/Application.java
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 public class Application {
 
 	public static void main(String[] args) {
-    	System.out.println("I'm running Spring Boot! Yay!");
+    	System.out.println("I'm running Spring Boot! Yay! :)");
 		SpringApplication.run(Application.class, args);
 	}
 }
